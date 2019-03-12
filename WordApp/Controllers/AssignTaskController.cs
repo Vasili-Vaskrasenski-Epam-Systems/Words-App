01aa3f5c54390af6cd80c66620ca750b16709889
Vasili Vaskrasenski <VasiliVaskrasenski@gmail.com>
2019-03-12 16:37:33 +0300

    TMP

---
+++ WordApp/Controllers/AssignTaskController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using WordApp.Models;
+
+namespace WordApp.Controllers
+{
+    public class AssignTaskController: BaseController
+    {
+        public AssignTaskController(IMapper mapper) : base(mapper)
+        {
+        }
+
+        [HttpPost]
+        public IActionResult AssignTask(AssignableWordTaskModel model)
+        {
+            //var entitiesToCreate = base.Mapper.
+            return Ok();
+        }
+    }
+}
---
+++ WordApp/Models/AssignableWordTaskModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using WordApp.Models.Base;
+
+namespace WordApp.Models
+{
+    public class AssignableWordTaskModel: BaseModel
+    {
+        public WordTaskModel Task { get; set; }
+        public List<UserModel> Users { get; set; }
+        public DateTime Deadline { get; set; }
+    }
+}
