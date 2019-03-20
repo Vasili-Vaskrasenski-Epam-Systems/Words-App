7baa01016b958a97d051b02c4b4e414359a6e2a9
Vasili Vaskrasenski <VasiliVaskrasenski@gmail.com>
2019-03-20 12:52:58 +0300

    - Added loader for words page;

--- WordApp/ClientApp/src/app/words/words.module.ts
+++ WordApp/ClientApp/src/app/words/words.module.ts
@@ -7,14 +7,15 @@ import { WordsService } from './words.service';
 import { CommonComponentsModule } from './../common/common-components.module';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material';
-import { MatButtonModule } from '@angular/material';
+import { MatSpinner } from '@angular/material';
 
 @NgModule({
   declarations: [
     WordListComponent,
-    WordEditorFormComponent
+    WordEditorFormComponent,
+    MatSpinner
   ],
-  imports: [FormsModule, BrowserModule, ReactiveFormsModule, CommonComponentsModule, MatTableModule, MatPaginatorModule, MatButtonModule],
+  imports: [FormsModule, BrowserModule, ReactiveFormsModule, CommonComponentsModule, MatTableModule, MatPaginatorModule],
   exports: [
     WordListComponent
   ],
