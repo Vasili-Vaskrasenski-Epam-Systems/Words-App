5562b89c948062046f9ce2d876a3372617afe4fe
Vasili Vaskrasenski <VasiliVaskrasenski@gmail.com>
2019-03-20 17:27:09 +0300

    Moved verb task details page to "smart" tables;

--- WordApp/ClientApp/src/app/tasks/verb-task/verb-task-details.component.ts
+++ WordApp/ClientApp/src/app/tasks/verb-task/verb-task-details.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { MatPaginator, MatTableDataSource } from '@angular/material';
 
 import { VerbTaskService } from './../services/verb-task.service';
 import { AssignVerbTaskService } from './../services/assign-verb-task.service';
 
 import { VerbTaskDetailModel } from './../models/verb-task-detail.model';
 import { AssignableVerbTaskModel } from './../models/assignable-verb-task.model';
+import { OrderedVerbTaskModel } from './../models/ordered-verb-task.model';
 
 @Component(
   {
@@ -14,6 +16,11 @@ import { AssignableVerbTaskModel } from './../models/assignable-verb-task.model'
   })
 export class VerbTaskDetailsComponent implements OnInit {
   public task: VerbTaskDetailModel;
+  public verbsDataSource: MatTableDataSource<OrderedVerbTaskModel>;
+  public assigneeDataSource: MatTableDataSource<AssignableVerbTaskModel>;
+
+  @ViewChild('verbsPaginator') verbsPaginator: MatPaginator;
+  @ViewChild('assigneePaginator') assigneePaginator: MatPaginator;
 
   constructor(private route: ActivatedRoute, private verbTaskService: VerbTaskService, private assignTaskService: AssignVerbTaskService) { }
 
@@ -21,14 +28,22 @@ export class VerbTaskDetailsComponent implements OnInit {
     this.route.params.subscribe(e => {
       this.verbTaskService.getTaskDetails(e['id']).subscribe(data => {
         this.task = data;
+        this.assigneeDataSource = new MatTableDataSource<AssignableVerbTaskModel>(this.task.assignees);
+        this.assigneeDataSource.paginator = this.assigneePaginator;
+
+        this.verbsDataSource = new MatTableDataSource<OrderedVerbTaskModel>(this.task.verbs);
+        this.verbsDataSource.paginator = this.verbsPaginator;
       });
     });
   }
 
   onUnAssign(assignedVerbTaskModel: AssignableVerbTaskModel) {
     this.assignTaskService.unassignTask(assignedVerbTaskModel).subscribe(e => {
-        var index = this.task.assignees.findIndex(t => t.id === e.id);
-        this.task.assignees.splice(index, 1);
+        var index = this.assigneeDataSource.data.findIndex(t => t.id === e.id);
+      this.assigneeDataSource.data.splice(index, 1);
+
+      this.assigneeDataSource = new MatTableDataSource<AssignableVerbTaskModel>(this.assigneeDataSource.data);
+      this.assigneeDataSource.paginator = this.assigneePaginator;
       },
       error => {
 
